9c3e255a33d3b103a8aef2e6f3aaad3fb394fea2
karthaze <akshatrathore.rathore@gmail.com>
2025-05-09 18:20:50 +0530

    ttt

--- backend/index.js
+++ backend/index.js
@@ -18,10 +18,7 @@ const app = express();
 
 connectDB();
 
-app.use(cors({
-  origin: process.env.CLIENT_URL,
-  credentials: true,
-}));
+app.use(cors());
 
 app.use(express.json({ limit: '20mb' }));
 app.use(express.urlencoded({ limit: '20mb', extended: true }));
