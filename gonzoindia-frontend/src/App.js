ff45e91692c48943f9a62dccd8ed648fc9779ed6
karthaze <akshatrathore.rathore@gmail.com>
2025-05-09 21:29:57 +0530

    fetch moved to feed card

--- gonzoindia-frontend/src/App.js
+++ gonzoindia-frontend/src/App.js
@@ -3,43 +3,9 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';  // Add your custom styles here
 import GJLandingPage from './pages/gj-landing-page/GJLandingPage';  // Make sure this path is correct
 import GJFeedPage from './pages/gj-feed-page/GJFeedPage';
-import { useDispatch } from 'react-redux';
-import axios from 'axios';
-import {setUser} from './features/auth/authSlice'
 
 
 const App = () => {
-
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        const backendURL = process.env.REACT_APP_BACKEND_URL;
-        console.log('Fetching user from:', backendURL);
-        try {
-          const res = await axios.get(`${backendURL}/auth/me`, {
-            withCredentials: true,
-          });
-          console.log('User data received:', res.data);
-          dispatch(setUser(res.data));
-          return;
-        } catch (authError) {
-          console.log('Auth/me failed, trying fallback:', authError);
-        }
-        const res = await axios.get(`${backendURL}/me`, {
-          withCredentials: true,
-        });
-        console.log('User data received from fallback:', res.data);
-        dispatch(setUser(res.data));
-      } catch (err) {
-        console.error('User not logged in', err);
-      }
-    };
-
-    fetchUser();
-  }, [dispatch]);
-  
 
   return (
     <Router>
